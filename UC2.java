6d7aa67c11b72471c5c4c7f8a683557a34a51861
likevishal87 <likevishalverma@gmail.com>
2022-08-15 14:36:22 +0530

    UC

---
+++ UC2.java
@@ -0,0 +1,9 @@
+
+public class UC2 {
+	public static void main (String[] args) {
+		int wage_per_hour = 20;
+		int full_day_hour = 8;
+		int wage = wage_per_hour*full_day_hour;
+		System.out.println("wage of employee for a day is: "+wage);
+ }
+}
